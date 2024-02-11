89a409d5d029c945cd1c892005493d36859b6054
Hector Macafonty <learnwithhector@gmail.com>
2024-02-11 23:42:43 +0000

    snail movement now implemented with rectangles

--- game.py
+++ game.py
@@ -12,7 +12,10 @@ ground_surface = pygame.image.load('./graphics/ground.png').convert()
 text_surface = test_font.render("My game", False, "Black")
 
 snail_surface = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rectangle = snail_surface.get_rect(bottomright=(600,300))
+
+player_surface = pygame.image.load('./graphics/player/player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -23,10 +26,12 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
+
+    snail_rectangle.x -= 4
+    if snail_rectangle.right <= 0:
+        snail_rectangle.left = 800
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
 
     pygame.display.update()
     clock.tick(60)
